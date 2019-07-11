d6851adea810163fee4be6f1e6a94196a25e5f47
Glitch (lc4-encryptor) <none>
2019-07-11 16:22:18 +0000

    🚡🐇 Checkpoint ./script.js:4879951/897 ./encryptor.html:4879951/550 ./style/style.css:4879951/39

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ import * as lc4 from "https://cdn.jsdelivr.net/gh/umcconnell/lc4/dist/main.js";
 let switcher = document.getElementById("encryptorFlip"),
     method = "encrypt",
     textareas = document.getElementsByTagName("textarea"),
-    methodLabels = document.querySelectorAll(".encryptor__label");
+    methodLabels = [...document.querySelectorAll(".encryptor__label")];
 
-
-function switchUi() {
+function switchUI() {
   // Flip textareas
   textareas[1].parentNode.insertBefore(textareas[1], textareas[0]);
   
   // Flip labels
+  methodLabels[0].parentNode.insertBefore(methodLabels[0], methodLabels[1]);
   methodLabels[1].parentNode.insertBefore(methodLabels[1], switcher);
-  methodLabels[0].parentNode.insert
+  methodLabels.reverse();
 }
 
 switcher.addEventListener("click", () => {
-  
-})
+  switchUI();
+});
